d575cf4952441b10c6e84f76041c46f0935a9b79
Hanna <h_kim@college.harvard.edu>
2018-07-12 11:42:57 +0100

    add home-page.jsx

--- client/src/components/home-page/home-page.jsx
+++ client/src/components/home-page/home-page.jsx
@@ -1,48 +1,24 @@
 import * as React from "react"
-import Navigation from "../navigation/navigation"
+// import Navigation from "../navigation/navigation"
 import Feed from "../feed"
-import Login from "../login/login"
-// import { diffQueryAgainstStore } from "apollo-cache-inmemory";
-
-// const auth = {
-//   isAuthenticated: false,
-// }
+import "./home-page.css"
 
 class HomePage extends React.Component {
-  state = {
-    isAuthenticated: localStorage.getItem('token'),
-  };
-
-  constructor(props) {
-    super(props)
-    this.rerender = this.rerender.bind(this)
-  }
-
-  rerender() {
-    this.forceUpdate();
-  }
-
   render() {
-    console.log(this.state.isAuthenticated);
-    return (
-      localStorage.getItem('token') ? (
-        <div>
-          <Navigation />
-          <Feed />
-          <button onClick={() => {
-            localStorage.removeItem('token')
-            this.rerender()
-          }}>
-            log out
-          </button>
-        </div>
-      ) : (
-          <div>
-            < div >
-              <Login rerender={this.rerender} />
-            </div >
+    return ( 
+      <div>
+        {/* <Navigation /> */}
+        <div className="body">
+          <div className="sidebar">
+            <div className="logo">
+              <img src={require ("../logo.png")}/>
+            </div>
+            {/* <div className="current-user"/>
+            <div className="logout"/> */}
           </div>
-        )
+          <div className="feed"><Feed/></div>
+        </div>
+      </div>
     )
   }
 }
