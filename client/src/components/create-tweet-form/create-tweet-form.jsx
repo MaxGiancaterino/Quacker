107459783cca3d25b4e848ee9137f2fa6f3f2670
Hanna <h_kim@college.harvard.edu>
2018-07-12 11:51:42 +0100

    add create-tweet-form folder

---
+++ client/src/components/create-tweet-form/create-tweet-form.jsx
@@ -0,0 +1,59 @@
+import * as React from "react"
+import gql from "graphql-tag"
+import { Mutation } from "react-apollo"
+import "./create-tweet-form.css"
+
+const CREATE_TWEET = gql`
+  mutation createTweet($data: TweetCreateInput!) {
+    createTweet(data: $data) {
+      text
+    }
+  }
+`
+
+class CreateTweetForm extends React.Component {
+  render() {
+    let input
+
+    return (
+      <div>
+        <Mutation mutation={CREATE_TWEET}>
+          {(createTweet, { data }) => {
+            return (
+              <div>
+                <form
+                  onSubmit={async e => {
+                    e.preventDefault()
+                    await createTweet({
+                      variables: {
+                        data: {
+                          author: {
+                            connect: {
+                              email: "harris@me.com"
+                            }
+                          },
+                          text: input.value
+                        }
+                      }
+                    })
+                    input.value = ""
+                    this.props.refetchFeedTweets()
+                  }}
+                >
+                  <input className="input-tweet" placeholder="What's happening?"
+                    ref={node => {
+                      input = node
+                    }}
+                  />
+                  <button className="search-button" type="submit">Quack!</button>
+                </form>
+              </div>
+            )
+          }}
+        </Mutation>
+      </div>
+    )
+  }
+}
+
+export default CreateTweetForm
